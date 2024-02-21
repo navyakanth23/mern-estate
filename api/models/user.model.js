c5b2fdfe27202f0f2e2c6b92b76459b55c1f2be7
Navya Kanth Meruga <navyakanth.meruga@gmail.com>
2024-02-21 13:49:28 -0600

    create user model

---
+++ api/models/user.model.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        requried: true,
+        unique: true,  
+    },
+    email: {
+        type: String,
+        requried: true,
+        unique: true,  
+    },
+    password: {
+        type: String,
+        requried: true,  
+    }
+}, { timestamps: true});
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
